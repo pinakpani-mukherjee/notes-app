6e9abcdd3cf2a47c92ce641410596ee4ea565dc2
Pinakpani Mukherjee <mukherjee.pinakpani14@gmail.com>
2022-11-26 13:15:10 +0530

    added data sources and repositories

---
+++ app/src/main/java/com/plcoding/cleanarchitecturenoteapp/feature_note/data/repository/noteRepositoryImpl.kt
@@ -0,0 +1,26 @@
+package com.plcoding.cleanarchitecturenoteapp.feature_note.data.repository
+
+import com.plcoding.cleanarchitecturenoteapp.feature_note.data.data_source.NoteDao
+import com.plcoding.cleanarchitecturenoteapp.feature_note.domain.model.Note
+import com.plcoding.cleanarchitecturenoteapp.feature_note.domain.repository.NoteRepository
+import kotlinx.coroutines.flow.Flow
+
+class noteRepositoryImpl(
+    private val dao: NoteDao
+) : NoteRepository {
+    override fun getNotes(): Flow<List<Note>> {
+        return dao.getNotes()
+    }
+
+    override suspend fun getNoteById(id: Int): Note? {
+        return dao.getNoteById(id)
+    }
+
+    override suspend fun insetNote(note: Note) {
+        return dao.insertNote(note)
+    }
+
+    override suspend fun deleteNote(note: Note) {
+        return dao.deleteNote(note)
+    }
+}
